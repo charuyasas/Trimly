42abc25342b4b7ac0d805d2a59601b640cc4f1dc
Yashodha <157001399+Yashodharajarathne@users.noreply.github.com>
2025-06-18 16:03:44 +0530

    [ TRIM-2 ] Add initial UI template to project

---
+++ storage/framework/views/a13f51d707bdb9fc26db3db66e6d434c.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>LOGIN</title>
+
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="<?php echo e(asset('assets/css/bootstrap1.min.css')); ?>" />
+    <link rel="stylesheet" href="<?php echo e(asset('assets/vendors/themefy_icon/themify-icons.css')); ?>" />
+    <link rel="stylesheet" href="<?php echo e(asset('assets/vendors/font_awesome/css/all.min.css')); ?>" />
+    <link rel="stylesheet" href="<?php echo e(asset('assets/vendors/scroll/scrollable.css')); ?>" />
+    <link rel="stylesheet" href="<?php echo e(asset('assets/css/metisMenu.css')); ?>" />
+    <link rel="stylesheet" href="<?php echo e(asset('assets/css/style1.css')); ?>" />
+    <link rel="stylesheet" href="<?php echo e(asset('assets/css/colors/default.css')); ?>" id="colorSkinCSS">
+</head>
+<body class="crm_body_bg">
+
+<section class="main_content dashboard_part large_header_bg" style="min-height: 100vh; display: flex; align-items: center; justify-content: center;padding-left: 0;">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8 col-lg-5">
+                <div class="modal-content cs_modal shadow rounded">
+                    <div class="modal-header justify-content-center theme_bg_1 rounded-top">
+                        <h5 class="modal-title text_white">Log in</h5>
+                    </div>
+                    <div class="modal-body px-4 py-4">
+                        <form>
+                            <div class="mb-3">
+                                <input type="email" class="form-control" placeholder="Enter your email">
+                            </div>
+                            <div class="mb-3">
+                                <input type="password" class="form-control" placeholder="Password">
+                            </div>
+                            <div class="d-grid mb-3">
+                                <a href="#" class="btn_1 text-center">Log In</a>
+                            </div>
+                            <p class="text-center">Need an account?
+                                <a href="#" data-toggle="modal" data-target="#sing_up" data-dismiss="modal">Sign Up</a>
+                            </p>
+                            <div class="text-center">
+                                <a href="#" class="pass_forget_btn" data-toggle="modal" data-target="#forgot_password" data-dismiss="modal">Forget Password?</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="text-center mt-4">
+                    <p class="small text-muted">2020 © Developed by
+                        <a href="#">ECHO DATA</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Scripts -->
+<script src="<?php echo e(asset('assets/js/jquery1-3.4.1.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/js/popper1.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/js/bootstrap1.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/js/metisMenu.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/vendors/scroll/perfect-scrollbar.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/vendors/scroll/scrollable-custom.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/js/custom.js')); ?>"></script>
+
+</body>
+</html>
+<?php /**PATH D:\Trimly\resources\views/auth/login.blade.php ENDPATH**/ ?>
