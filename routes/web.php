bbbb02ad6edb4bb4d941a6478c26668ae4f1b7d6
sajini96 <sajinipussella@gmail.com>
2025-06-18 17:05:58 +0530

    [ Trim-2 ] Create dashboard UI

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('auth.login');
+});
+
+Route::get('/', function () {
+    return view('index');
 });
 
