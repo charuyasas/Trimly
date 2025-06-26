89f19be009f5a0706a79d581a8b0c283761e7e1d
sajini96 <sajinipussella@gmail.com>
2025-06-26 11:08:08 +0530

    [TRIM-8] Sales Invoice

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -32,6 +32,13 @@
     <link rel="stylesheet" href="{{ asset('assets/css/colors/default.css') }}" id="colorSkinCSS">
     <script src="{{ asset('assets/js/jquery1-3.4.1.min.js') }}"></script> 
 
+    <!-- jQuery UI for Autocomplete -->
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
+
+    
+
+
     
 </head>
 <body class="crm_body_bg">
