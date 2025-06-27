42db3c8e3efdcde4b63cbd80c632b58916bf3aed
Yashodha <157001399+Yashodharajarathne@users.noreply.github.com>
2025-06-27 17:31:07 +0530

    [TRIM-6] Continued booking calendar improvements

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -37,16 +37,6 @@
             </div>
             </a>
         </li>
-         <li class="">
-            <a  href="/bookings" aria-expanded="false">
-              <div class="nav_icon_small">
-                <img src="{{ asset('assets/img/menu-icon/15.svg') }}" alt="">
-            </div>
-            <div class="nav_title">
-                <span>Bookings</span>
-            </div>
-            </a>
-        </li>
         <li class="">
             <a  href="/bookings" aria-expanded="false">
               <div class="nav_icon_small">
