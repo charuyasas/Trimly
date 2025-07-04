1e7a0716870b6259601ac9783443df4e1b03ed54
sajini96 <sajinipussella@gmail.com>
2025-07-04 10:12:02 +0530

    [TRIM-14] Auto create ledger code when Employee Registration

--- app/Http/Controllers/API/EmployeeController.php
+++ app/Http/Controllers/API/EmployeeController.php
@@ -11,7 +11,8 @@ use App\UseCases\Employee\Requests\EmployeeRequest;
 use App\UseCases\Employee\ShowEmployeeInteractor;
 use App\UseCases\Employee\StoreEmployeeInteractor;
 use App\UseCases\Employee\updateEmployeeInteractor;
-use Illuminate\Http\Request;
+use App\UseCases\PostingAccount\Requests\PostingAccountRequest;
+use App\UseCases\PostingAccount\StorePostingAccountInteractor;
 
 class EmployeeController extends Controller
 {
@@ -20,9 +21,15 @@ class EmployeeController extends Controller
         return $listEmployeeInteractor->execute();
     }
 
-    public function store(StoreEmployeeInteractor $storeEmployeeInteractor)
+    public function store(StoreEmployeeInteractor $storeEmployeeInteractor, StorePostingAccountInteractor $storePostingAccountInteractor)
     {
-        $newEmployee = $storeEmployeeInteractor->execute(EmployeeRequest::validateAndCreate(request()));
+        $ledgerCode = $this->createLedgerCode($storePostingAccountInteractor);
+        $employeeData = array_merge(
+            request()->all(),
+            ['ledger_code' => $ledgerCode]
+        );
+
+        $newEmployee = $storeEmployeeInteractor->execute(EmployeeRequest::validateAndCreate($employeeData));
         return response()->json($newEmployee , 201);
     }
 
@@ -46,6 +53,20 @@ class EmployeeController extends Controller
     public function loadEmployeeDropdown(LoadEmployeemDropdownInteractor $loadEmployeemDropdownInteractor)
     {
         return response()->json($loadEmployeemDropdownInteractor->execute(request('search_key')));
+    }
+
+    public function createLedgerCode(StorePostingAccountInteractor $storePostingAccountInteractor)
+    {
+        $data = [
+            'posting_code'     => null,
+            'posting_account'  => 'Employee Account',
+            'main_code'        => 4,
+            'heading_code'     => 8,
+            'title_code'       => 9,
+        ];
+
+        $newPostingAccount = $storePostingAccountInteractor->execute(PostingAccountRequest::validateAndCreate($data));
+        return $newPostingAccount['ledger_code'];
     }
 
 
