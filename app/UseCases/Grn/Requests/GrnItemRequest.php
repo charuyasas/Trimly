dacc37740281585f2ea4318ddec68f3a398cdb66
Yashodha <157001399+Yashodharajarathne@users.noreply.github.com>
2025-07-16 15:20:00 +0530

    [TRIM-21] List GRN

--- app/UseCases/Grn/Requests/GrnItemRequest.php
+++ app/UseCases/Grn/Requests/GrnItemRequest.php
@@ -2,21 +2,35 @@
 
 namespace App\UseCases\Grn\Requests;
 
+use Spatie\LaravelData\Attributes\Validation\Rule as SpatieRule;
 use Spatie\LaravelData\Data;
 
 class GrnItemRequest extends Data
 {
+    #[SpatieRule('required', 'string')]
     public string $item_id;
+
+    #[SpatieRule('required', 'string')]
     public string $item_name;
+
+    #[SpatieRule('required', 'numeric', 'min:0')]
     public float $qty;
+
+    #[SpatieRule('nullable', 'numeric', 'min:0')]
     public float $foc;
 
+    #[SpatieRule('required', 'numeric', 'min:0')]
     public float $price;
 
+    #[SpatieRule('nullable', 'numeric', 'min:0')]
     public ?float $margin;
 
+    #[SpatieRule('nullable', 'numeric', 'min:0')]
     public ?float $discount;
+
+    #[SpatieRule('required', 'numeric', 'min:0')]
     public float $final_price;
 
+    #[SpatieRule('required', 'numeric', 'min:0')]
     public float $subtotal;
 }
