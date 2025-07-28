4b3d9cc58b1141ac98262b2957003c2aa6c45d01
sajini96 <sajinipussella@gmail.com>
2025-07-28 17:32:21 +0530

    [TRIM-8] Add journal entries and stock movement into invoice panel finish

--- app/Constance/AccountsLedgerCodes.php
+++ app/Constance/AccountsLedgerCodes.php
@@ -5,6 +5,10 @@ namespace App\Constance;
 class AccountsLedgerCodes
 {
     const LEDGER_CODES = [
-        'MainStore' => '1-2-6-1000'
+        'MainStore' => '1-2-6-1000',
+        'Cash in Hand' => '1-2-3-1001',
+        'Income' => '4-8-18-1002',
+        'expense' => '4-8-10-1003',
+        'Sales Revenue' => '5-16-19-1004',
     ];
 }
--- database/seeders/PostingAccountSeeder.php
+++ database/seeders/PostingAccountSeeder.php
@@ -10,5 +10,9 @@ class PostingAccountSeeder extends Seeder
     public function run(): void
     {
         PostingAccount::create(['posting_code' => 1000, 'posting_account' => 'Inventory', 'main_code' => 1, 'heading_code' => 2, 'title_code' => 6]);
+        PostingAccount::create(['posting_code' => 1001, 'posting_account' => 'Cash in Hand', 'main_code' => 1, 'heading_code' => 2, 'title_code' => 3]);
+        PostingAccount::create(['posting_code' => 1002, 'posting_account' => 'Income', 'main_code' => 4, 'heading_code' => 8, 'title_code' => 18]);
+        PostingAccount::create(['posting_code' => 1003, 'posting_account' => 'Expenses', 'main_code' => 4, 'heading_code' => 8, 'title_code' => 10]);
+        PostingAccount::create(['posting_code' => 1004, 'posting_account' => 'Sales Revenue', 'main_code' => 5, 'heading_code' => 16, 'title_code' => 19]);
     }
 }
