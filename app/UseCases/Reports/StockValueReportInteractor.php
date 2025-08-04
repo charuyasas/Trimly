e07a2d31cb77afba0af1524c0c63a14383b6910b
Yashodha <157001399+Yashodharajarathne@users.noreply.github.com>
2025-08-04 14:13:20 +0530

    [TRIM-41] Stock value report modifications

--- app/UseCases/Reports/StockValueReportInteractor.php
+++ app/UseCases/Reports/StockValueReportInteractor.php
@@ -8,10 +8,16 @@ class StockValueReportInteractor
 {
     public function execute()
     {
-        return DB::table('items')
+        return DB::table('categories')
+            ->leftJoin('sub_categories', 'sub_categories.category_id', '=', 'categories.id')
+            ->leftJoin('items', 'items.sub_category_id', '=', 'sub_categories.id')
             ->leftJoin('stock_sheets', 'stock_sheets.item_code', '=', 'items.id')
             ->select(
-                'items.id',
+                'categories.id as category_id',
+                'categories.name as category_name',
+                'sub_categories.id as sub_category_id',
+                'sub_categories.name as sub_category_name',
+                'items.id as item_id',
                 'items.code',
                 'items.description',
                 'items.average_cost',
@@ -20,9 +26,21 @@ class StockValueReportInteractor
                 DB::raw('COALESCE(SUM(stock_sheets.debit), 0) - COALESCE(SUM(stock_sheets.credit), 0) as stock_balance'),
                 DB::raw('items.average_cost * (COALESCE(SUM(stock_sheets.debit), 0) - COALESCE(SUM(stock_sheets.credit), 0)) as total_stock_value')
             )
-            ->groupBy('items.id', 'items.code', 'items.description', 'items.average_cost')
-            ->havingRaw('(COALESCE(SUM(stock_sheets.debit), 0) - COALESCE(SUM(stock_sheets.credit), 0)) > 0')  //show only items with stock balance > 0
+            ->groupBy(
+                'categories.id',
+                'categories.name',
+                'sub_categories.id',
+                'sub_categories.name',
+                'items.id',
+                'items.code',
+                'items.description',
+                'items.average_cost'
+            )
+            ->havingRaw('(COALESCE(SUM(stock_sheets.debit), 0) - COALESCE(SUM(stock_sheets.credit), 0)) > 0')
+            ->orderBy('categories.name')
+            ->orderBy('sub_categories.name')
             ->orderBy('items.code')
             ->get();
     }
+
 }
